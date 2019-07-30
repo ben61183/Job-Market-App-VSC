f431738ebac106d1836940cf27f6e32d93c1094d
Rohan Warrier <Rohan61175@Mastek.Com>
2019-07-30 14:37:39 +0100

    Vacancy Component 1

--- jobMarket/src/app/vacancy/vacancy.component.ts
+++ jobMarket/src/app/vacancy/vacancy.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vacancy.component.css']
 })
 export class VacancyComponent implements OnInit {
+  
+  vacancyId:number;
+  company:string;
+  description:string;
+  job_type:boolean;
+  link:string;
+  location:string;
+  postTime:string;
+  salary:number;
+  title: string;
 
-  constructor() { }
+  thisRole: String;
 
+  constructor() { 
+  
+  this.vacancyId=22;
+  this.company="default company";
+  this.description="default description";
+  this.job_type=false;
+  this.link="default website";
+  this.location="default location";
+  this.postTime="default post time";
+  this.salary=0;
+  this.title="";
+
+  this.thisRole= 
+  {
+    
+  }
+
+  }
   ngOnInit() {
+
   }
 
 }
