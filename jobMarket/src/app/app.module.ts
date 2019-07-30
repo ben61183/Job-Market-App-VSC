bf8c5030e5e0e2cb1824049d7c00f75a75cf21a3
Ben Gregory <Ben61183@Mastek.Com>
2019-07-30 10:00:16 +0100

    bad role component removed

--- jobMarket/src/app/app.module.ts
+++ jobMarket/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RoleComponent } from './role/role.component';
 import { VacancyComponent } from './vacancy/vacancy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoleComponent,
     VacancyComponent
   ],
   imports: [
