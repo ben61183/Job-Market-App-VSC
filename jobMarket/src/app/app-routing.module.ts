de2ad9fa56299cc44d3fc7afeacc109e253901d6
Joseph Wilson <Joseph61179@Mastek.Com>
2019-08-01 10:37:27 +0100

    chart.js

--- jobMarket/src/app/app-routing.module.ts
+++ jobMarket/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RoleDashboardComponent } from './role-dashboard/role-dashboard.component';
 import { RoleComponent } from './role/role.component';
 import { VacancyComponent } from './vacancy/vacancy.component';
-
+import { RoleAdminComponent} from './role-admin/role-admin.component'
 
 const routes: Routes = [
   {path: "role-dashboard", component: RoleDashboardComponent},
   {path: "role/:roleId", component: RoleComponent},
   {path: "vacancy-dashboard", component: VacancyComponent},
+  {path: "role-admin", component: RoleAdminComponent}
   // {path: "login", component: UserComponent}
 ];
 
