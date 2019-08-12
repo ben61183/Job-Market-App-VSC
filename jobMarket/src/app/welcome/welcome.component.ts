48fc287545ec3b9ba29f36299f15080d189b84c6
Ben Gregory <Ben61183@Mastek.Com>
2019-08-12 10:36:49 +0100

    welcome

--- jobMarket/src/app/welcome/welcome.component.ts
+++ jobMarket/src/app/welcome/welcome.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { UserIdService } from '../user-id.service';
+import { CompanyIdService } from '../company-id.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css']  ,
+  providers: [UserIdService,CompanyIdService]
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  myUserId:number
+  myCompanyId:number
 
-  ngOnInit() {
+  constructor(private uidSer:UserIdService, private cidSer:CompanyIdService) { }
+
+  ngOnInit() {  
+    this.uidSer.currentUserId.subscribe(myUserId => this.myUserId = myUserId)
+    this.cidSer.currentCompanyId.subscribe(myCompanyId=>this.myCompanyId=myCompanyId)
   }
 
 }
